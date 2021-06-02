04eb672fedc73b9c63c84712aea5fa7131886120
Jessica <jessica.giancola@student.univaq.it>
2021-06-02 10:34:33 +0200

    aggiunto file per il nome del paziente + invio a database del reminder della terapia

--- publisher.py
+++ publisher.py
@@ -2,6 +2,7 @@
 import requests as req
 import redis
 import time
+import json
 
 # Nome del canale su cui invio i parametri (deve corrispondere al canale su cui Lindaproxy ascolta)
 CH_OUT = 'TO_AGENT'
@@ -9,36 +10,58 @@ CH_OUT = 'TO_AGENT'
 
 def getParameters(parameters, redisInstance, name):
     for parameter in parameters:
-        #request_url = "http://www.lorenzodelauretis.it/tesi/index.php?nome=" + name + "&" + parameter[1] + "=1"
-        request_url = "http://localhost/prova.php?nome=" + name + "&" + parameter[1] + "=1"
-        response = req.get(request_url)
-        prologParam = parameter[0]+"("+response.text+")"
-        redisInstance.publish(CH_OUT, prologParam)  # Pubblico sul canale il parametro raccolto
-        print(prologParam)
+        request_url = "http://www.lorenzodelauretis.it/tesi/index.php?nome=" + name + "&" + parameter[1] + "=1"
+        # request_url = "http://localhost/prova.php?nome=" + name + "&" + parameter[1] + "=1"
+        response = req.get (request_url)
+        prologParam = parameter[0] + "(" + response.text + ")"
+        redisInstance.publish (CH_OUT, prologParam)  # Pubblico sul canale il parametro raccolto
+        print (prologParam)
 
 
-#def run(patientName):
-R = redis.Redis()  # Istanza 1 di Redis
-patientName = "Lorenzo De Lauretis"
-name_url = patientName.replace(" ", "%20")
+def run(patientName):
+    R = redis.Redis()  # Istanza 1 di Redis
+    # patientName = "Lorenzo De Lauretis"
+    name_url = patientName.replace(" ", "%20")
 
-parameters = [["saturazione", "get_saturazione"], ["battito", "get_battito"], ["presminima", "get_presminima"], ["presmassima", "get_presmassima"], ["temperatura", "get_temperatura"], ["peso", "get_peso"], ["altezza", "get_altezza"], ["stato", "get_stato"]]
+    parameters = [["saturazione", "get_saturazione"], ["battito", "get_battito"], ["presminima", "get_presminima"], ["presmassima", "get_presmassima"], ["temperatura", "get_temperatura"], ["peso", "get_peso"], ["altezza", "get_altezza"], ["stato", "get_stato"]]
 
-# Inserisco le malattie pregresse
-#request_url = "http://www.lorenzodelauretis.it/tesi/index.php?nome=" + name_url + "&get_patologia=1"
-# patologies = req.get(request_url)
-# patologiesList = patologies.text.split(";")
-#
-#
-# for patology in patologiesList:
-#     if patology != '':
-#         prolog = "patologia(" + patology + ")"
-#         R.publish(CH_OUT, prolog)
+    # # Inserisco le malattie pregresse
+    #
+    # request_url = "http://www.lorenzodelauretis.it/tesi/index.php?nome=" + name_url + "&get_patologia=1"
+    # patologies = req.get(request_url)
+    # patologiesList = patologies.text.split(";")
+    #
+    # for patology in patologiesList:
+    #     if patology != '':
+    #         prolog = "patologia(" + patology + ")"
+    #         R.publish(CH_OUT, prolog)
+    #
+    # # Inserisco la terapia
+    #
+    # request_url = "http://www.lorenzodelauretis.it/tesi/index.php?nome=" + name_url + "&get_terapia=1"
+    # therapies = req.get(request_url)
+    # therapiesList = json.loads(therapies)
+    #
+    # for therapy in therapiesList['terapia']:
+    #     ora = therapy['orario'][0:2]
+    #     minuti = therapy['orario'][-2:]
+    #     if therapy != '':
+    #         prolog = "terapia(medicine(" + therapy['medicina'] + "," + therapy['quantita'] + "),'" + ora + "','" + minuti + "')"
+    #         print("stringa prolog " + prolog)
+    #         R.publish(CH_OUT, prolog)
 
-for i in range(0, 10):
-    getParameters(parameters, R, name_url)
-    time.sleep(10)
+    for i in range(0, 10):
+        getParameters(parameters, R, name_url)
+        time.sleep(10)
 
-#
-# if __name__ == '__main__':
-#     run('Lorenzo De Lauretis')  # Passo come parametro il nome del paziente
+
+if __name__ == '__main__':
+
+    try:
+        file = open('nome_paziente.txt', 'r')
+        nome = file.read()
+        print(nome)
+        file.close()
+        run(nome)  # Passo come parametro il nome del paziente
+    except IOError:
+        print("File not accessible")
--- subscriber.py
+++ subscriber.py
@@ -15,21 +15,22 @@ def clearList(redisInstance, channel):
     return
 
 
-def sendResponse(message):
+def sendResponse(message, nome):
     # Elaboro la risposta e la invio al database
     splitted_message = message.split(SEPARATOR)
+    urlbase = "http://www.lorenzodelauretis.it/tesi/index.php?"
     to_send = ""
     for case in switch(splitted_message[0]):
         if case('misura'):
             to_send = "Devi misurare " + splitted_message[1]
             break
         if case('medicina'):
-            to_send = "Devi prendere la medicina " + splitted_message[1] + " quantita " + splitted_message[2]
+            to_send = "remindermedicina=1&nome=" + nome.replace(" ", "%20") + "&nomemedicina=" + splitted_message[1] + "&quantita=" + splitted_message[2] + "&message=Devi prendere la medicina " + splitted_message[1] + " quantita " + splitted_message[2]
         else:
             print('Risposta non valida')
 
     to_send_url = to_send.replace(" ", "%20")
-    url = "http://www.lorenzodelauretis.it/tesi/index.php?message=" + to_send_url
+    url = urlbase + to_send_url
     #url = "http://localhost/prova.php?message=" + to_send_url
     print(url)
     req.get(url)
@@ -37,17 +38,25 @@ def sendResponse(message):
     return
 
 
-#def runSubscriber():
-R = redis.Redis()
-clearList(R, CH_IN)
+def run(nome):
+    R = redis.Redis()
+    clearList(R, CH_IN)
 
-while True:
-    response = R.lpop(CH_IN)
-    if response:
-        # Ho ricevuto un messaggio
-        sendResponse(response.decode('utf-8'))
-        print(response.decode('utf-8'))
+    while True:
+        response = R.lpop(CH_IN)
+        if response:
+            # Ho ricevuto un messaggio
+            sendResponse(response.decode('utf-8'), nome)
+            print(response.decode('utf-8'))
 
 
-# if __name__ == '__main__':
-#     runSubscriber()
+if __name__ == '__main__':
+
+    try:
+        file = open('nome_paziente.txt', 'r')
+        nome = file.read()
+        print(nome)
+        file.close()
+        run(nome)  # Passo come parametro il nome del paziente
+    except IOError:
+        print("File not accessible")
