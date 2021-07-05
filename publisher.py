5fba217fb309262f273c79f2d87e102edeffa422
Jessica <jessica.giancola@student.univaq.it>
2021-07-05 16:13:12 +0200

    rifinimenti

--- publisher.py
+++ publisher.py
@@ -3,8 +3,8 @@ import redis
 import time
 import json
 
-# Nome del canale su cui invio i parametri (deve corrispondere al canale su cui Lindaproxy ascolta)
-CH_OUT = 'TO_AGENT'
+CH_OUT = 'TO_AGENT'  # Nome del canale su cui invio i parametri (deve corrispondere al canale su cui Lindaproxy ascolta)
+LIST_SEPARATOR = ';'
 
 
 def makeAtomic(s, therapy=False):
@@ -21,69 +21,66 @@ def getParameters(parameters, redisInstance, url_base):
     for parameter in parameters:
         request_url = url_base + "&" + parameter[1] + "=1"
         response = req.get(request_url).text
-        if parameter[0] == 'stato' or parameter[0] == 'sensazione':
-            array = response.lower().split(";")
-            for feel in array:
-                if feel:
-                    prologParam = parameter[0] + "(" + feel + ")"
-                    prolog = makeAtomic(prologParam)
-                    redisInstance.publish(CH_OUT, prolog)  # Pubblico sul canale il parametro raccolto
+        if response:
+            if parameter[0] == 'stato' or parameter[0] == 'sensazione':
+                array = response.lower().split(LIST_SEPARATOR)
+                for feel in array:
+                    if feel:
+                        prologParam = parameter[0] + "(" + feel + ")"
+                        prolog = makeAtomic(prologParam)
+                        redisInstance.publish(CH_OUT, prolog)  # Pubblico sul canale il parametro raccolto
 
-        else:
-            prologParam = parameter[0] + "(" + response + ")"
-            prolog = makeAtomic(prologParam)
-            redisInstance.publish(CH_OUT, prolog)  # Pubblico sul canale il parametro raccolto
-
-        print(prologParam)
+            else:
+                prologParam = parameter[0] + "(" + response + ")"
+                prolog = makeAtomic(prologParam)
+                redisInstance.publish(CH_OUT, prolog)  # Pubblico sul canale il parametro raccolto
+            print(prologParam)
 
 
 def run(patientName):
     R = redis.Redis()  # Istanza 1 di Redis
-    name_url = patientName.replace(" ", "%20")
-    #url_base = "http://www.lorenzodelauretis.it/tesi/index.php?nome=" + name_url
-    url_base = "http://localhost/prova.php?"
+    url_base = "http://www.lorenzodelauretis.it/tesi/index.php?nome=" + patientName
+    #url_base = "http://localhost/prova.php?"
 
     parameters = [["anni", "get_eta"], ["altezza", "get_altezza"], ["peso", "get_peso"], ["temperatura", "get_temperatura"], ["saturazione", "get_saturazione"], ["battito", "get_battito"], ["presminima", "get_presminima"], ["presmassima", "get_presmassima"], ["stato", "get_stato"], ["sensazione", "get_sensazione"]]
-    #parameters = [["sensazione", "get_sensazione"]]
-    # # Inserisco le malattie pregresse
-    #
-    # request_url = url_base + "&get_patologia=1"
-    # patologies = req.get(request_url)
-    # patologiesList = patologies.text.split(";")
-    #
-    #
-    # for patology in patologiesList:
-    #     if patology != '':
-    #         prolog = "patologia('" + patology.lower() + "')"
-    #         prologAtomic = makeAtomic(prolog)
-    #         R.publish(CH_OUT, prologAtomic)
-    # print('inserite patologie')
-    #
-    # time.sleep(5)
-    #
-    # # Inserisco la terapia
-    #
-    # request_url = url_base + "&get_terapia=1"
-    # therapies = req.get(request_url).text
-    # print(therapies)
-    # therapiesList = json.loads(therapies)
-    #
-    # for therapy in therapiesList:
-    #     orario = therapy['orario'].split(":")
-    #
-    #     if therapy != '':
-    #         prolog = "'" + therapy['medicina'].lower() + "','" + therapy['quantita'].lower() + "'," + orario[0] + "," + orario[1]
-    #         prologAtomic = makeAtomic(prolog, True)
-    #         prologAtomic = "terapia(" + prologAtomic + ")"
-    #         print("stringa prolog " + prolog + " e " + prologAtomic)
-    #         R.publish(CH_OUT, prologAtomic)
-    # print('inserita terapia')
-    #
-    # time.sleep(5)
+
+    # Inserisco le malattie pregresse
+
+    request_url = url_base + "&get_patologia=1"
+    patologies = req.get(request_url)
+    if patologies:
+        patologiesList = patologies.text.split(LIST_SEPARATOR)
+        for patology in patologiesList:
+            if patology:
+                prolog = "patologia('" + patology.lower() + "')"
+                prologAtomic = makeAtomic(prolog)
+                R.publish(CH_OUT, prologAtomic)
+
+    print('Inviate patologie pregresse')
+
+    time.sleep(5)
+
+    # Inserisco la terapia
+
+    request_url = url_base + "&get_terapia=1"
+    therapies = req.get(request_url).text
+    if therapies:
+        therapiesList = json.loads(therapies)
+        for therapy in therapiesList:
+            orario = therapy['orario'].split(":")
+            if therapy:
+                prolog = "'" + therapy['medicina'].lower() + "','" + therapy['quantita'].lower() + "'," + orario[0] + "," + orario[1]
+                prologAtomic = makeAtomic(prolog, True)
+                prologAtomic = "terapia(" + prologAtomic + ")"
+                R.publish(CH_OUT, prologAtomic)
+
+    print('Inviata terapia')
+
+    time.sleep(5)
 
     for i in range(0, 10):
         getParameters(parameters, R, url_base)
-        print(' inviati parametri n ' + str(i))
+        print('Inviati parametri n ' + str(i))
         time.sleep(10)
 
 
@@ -91,9 +88,13 @@ if __name__ == '__main__':
 
     try:
         file = open('nome_paziente.txt', 'r')
-        nome = file.read()  # leggo il nome del paziente dal file
+        patientName = file.read()  # Leggo il nome del paziente dal file
         file.close()
-        run(nome)  # Passo come parametro il nome del paziente
+        if patientName:
+            name = ' '.join(patientName.split())  # Elimina eventuali spazi in eccesso
+            run(name)  # Passo come parametro il nome del paziente
+        else:
+            print("Nome del paziente non presente")
 
     except IOError:
         print("File not accessible")
--- subscriber.py
+++ subscriber.py
@@ -1,11 +1,12 @@
 import redis
 import requests as req
 from switchcase import switch
-import time
 
-# Canale su cui il MAS invia i messaggi
-CH_IN = 'RESPONSE'
+CH_IN = 'RESPONSE'  # Canale su cui il MAS invia i messaggi
 SEPARATOR = '+'
+LOW = 'low'
+NORMAL = 'normal'
+HIGH = 'high'
 
 
 # Svuota la lista dei messaggi inviati dal MAS
@@ -17,7 +18,7 @@ def clearList(redisInstance, channel):
 
 # Elaboro la risposta e la invio al database
 def sendResponse(message, url_base):
-    splitted_message = message.split(SEPARATOR)
+    splitted_message = message.replace("_", " ").split(SEPARATOR)
     to_send = ""
     mes = ""
     for case in switch(splitted_message[0]):
@@ -26,8 +27,8 @@ def sendResponse(message, url_base):
             break
 
         if case('medicina'):
-            to_send = "&remindermedicina=1&nomemedicina=" + splitted_message[1].replace("-", " ") + "&quantita=" \
-                      + splitted_message[2].replace("-", " ") + "&message=Devi prendere la medicina " \
+            to_send = "&remindermedicina=1&nomemedicina=" + splitted_message[1] + "&quantita=" \
+                      + splitted_message[2] + "&message=Devi prendere la medicina " \
                       + splitted_message[1] + " quantita " + splitted_message[2]
             break
 
@@ -44,75 +45,86 @@ def sendResponse(message, url_base):
             break
 
         if case('saturazione'):
+
             for feed in switch(splitted_message[1]):
-                if feed('high'):
+                if feed(HIGH):
                     mes = "Hai problemi di ossigenazione"
                     break
-                if feed('normal'):
+                if feed(NORMAL):
                     mes = "Non hai problemi di ossigenazione"
                     break
-                if feed('low'):
+                if feed(LOW):
                     mes = "Hai un leggero problema di ossigenazione"
                     break
+
             to_send = "&feed=" + splitted_message[0] + "&message=" + mes
             break
 
         if case('battito'):
+
             for feed in switch(splitted_message[1]):
-                if feed('low'):
+                if feed(LOW):
                     mes = "La tua frequenza cardiaca presenta una leggera bradicardia"
                     break
-                if feed('normal'):
+                if feed(NORMAL):
                     mes = "La tua frequenza cardiaca non presenta problemi"
                     break
-                if feed('high'):
+                if feed(HIGH):
                     mes = "La tua frequenza cardiaca presenta una leggera tachicardia"
                     break
+
             to_send = "&feed=" + splitted_message[0] + "&message=" + mes
             break
 
         if case('presminima'):
+
             for feed in switch(splitted_message[1]):
-                if feed('low'):
+                if feed(LOW):
                     mes = "La tua pressione minima presenta una ipotensione"
                     break
-                if feed('normal'):
+                if feed(NORMAL):
                     mes = "La tua pressione minima non presenta problemi"
                     break
-                if feed('high'):
+                if feed(HIGH):
                     mes = "La tua pressione minima presenta una ipertensione"
                     break
+
             to_send = "&feed=" + splitted_message[0] + "&message=" + mes
             break
 
         if case('presmassima'):
+
             for feed in switch(splitted_message[1]):
-                if feed('low'):
+                if feed(LOW):
                     mes = "La tua pressione massima presenta una ipotensione"
                     break
-                if feed('normal'):
+                if feed(NORMAL):
                     mes = "La tua pressione massima non presenta problemi"
                     break
-                if feed('high'):
+                if feed(HIGH):
                     mes = "La tua pressione massima presenta una ipertensione"
                     break
+
             to_send = "&feed=" + splitted_message[0] + "&message=" + mes
             break
 
         if case('temperatura'):
+
             for feed in switch(splitted_message[1]):
-                if feed('low'):
+                if feed(LOW):
                     mes = "Hai qualche linea di febbre, ti consiglio di riposare"
                     break
-                if feed('ok'):
+                if feed(NORMAL):
                     mes = "Non hai febbre"
                     break
                 else:
                     break
+
             to_send = "&feed=" + splitted_message[0] + "&message=" + mes
             break
 
         if case('doctor'):
+
             for feed in switch(splitted_message[1]):
                 if feed('prontosoccorso'):
                     mes = "Devi correre al pronto soccorso"
@@ -121,7 +133,7 @@ def sendResponse(message, url_base):
                     mes = "Il dottore suggerisce di ridurre il consumo di acqua"
                     break
                 else:
-                    mes = "Il dottore suggerisce di prendere " + splitted_message[1].replace("_", " ")
+                    mes = "Il dottore suggerisce di prendere " + splitted_message[1]
 
             to_send = "&feed=medico&message=" + mes
             break
@@ -134,9 +146,7 @@ def sendResponse(message, url_base):
             to_send = ''
             print('Risposta non valida: ' + message)
 
-    to_send_url = url_base + to_send
-    url = to_send_url.replace(" ", "%20")
-    print(url)
+    url = url_base + to_send
     req.get(url)
     return
 
@@ -144,8 +154,9 @@ def sendResponse(message, url_base):
 def run(patientName):
     R = redis.Redis()
     clearList(R, CH_IN)
-    #url_base = "http://www.lorenzodelauretis.it/tesi/index.php?nome=" + patientName
-    url_base = "http://localhost/prova.php?"
+    url_base = "http://www.lorenzodelauretis.it/tesi/index.php?nome=" + patientName
+    #url_base = "http://localhost/prova.php?"
+
     while True:
         response = R.lpop(CH_IN)
         if response:
@@ -158,9 +169,13 @@ if __name__ == '__main__':
 
     try:
         file = open('nome_paziente.txt', 'r')
-        patientName = file.read()  # leggo il nome del paziente dal file
+        patientName = file.read()  # Leggo il nome del paziente dal file
         file.close()
-        run(patientName)  # Passo come parametro il nome del paziente
+        if patientName:
+            name = ' '.join(patientName.split())  # Elimina eventuali spazi in eccesso
+            run(name)  # Passo come parametro il nome del paziente
+        else:
+            print("Nome del paziente non presente")
 
     except IOError:
         print("File not accessible")
